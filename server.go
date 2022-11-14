ab0ed021ed61c637610d367d2aa9d6a16d5f69da
Akash JS <akashjs710@gmail.com>
2022-11-14 14:14:09 +0530

    updated pg.go

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net"
 
+	"github.com/jsakash/gRPC_MiniProject/chat"
 	"google.golang.org/grpc"
 )
 
@@ -13,7 +14,11 @@ func main() {
 		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
 
+	s := chat.Server{}
+
 	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
