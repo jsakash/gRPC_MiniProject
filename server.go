426fc2c8d22d139564cc0016629f496f242fcefa
Akash JS <akashjs710@gmail.com>
2022-11-14 12:23:02 +0530

    initial commit

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
+
+}
