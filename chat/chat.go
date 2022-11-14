0f89bb093be36815cca835658fa043745f76bdd8
Akash JS <akashjs710@gmail.com>
2022-11-14 14:00:41 +0530

    added chat.go file

---
+++ chat/chat.go
@@ -0,0 +1,15 @@
+package chat
+
+import (
+	"context"
+	"log"
+)
+
+type Server struct {
+}
+
+func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
+	log.Printf("Received message body from client: %s", message.Body)
+
+	return &Message{Body: "Hello from the server"}, nil
+}
--- chat/chat.pb.go
+++ chat/chat.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v3.21.9
 // source: chat.proto
 
-package gRPC_MiniProject
+package chat
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
